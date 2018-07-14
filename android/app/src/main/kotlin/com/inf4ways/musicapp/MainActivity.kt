017ff3cfca98e9983e5a158726d256e68ac7801a
Troinux <201606327@keyaka.ul.ac.za>
2018-07-14 19:22:15 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/inf4ways/musicapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.inf4ways.musicapp
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity(): FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
